fc5bb0c96579d893d5fb4fb26e00c8cdf340e52e
shuvronilmondal3 <141385195+shuvronilmondal3@users.noreply.github.com>
2026-01-15 08:08:32 +0530

    initial project setup

---
+++ src/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+port = process.env.PORT || 4044
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
